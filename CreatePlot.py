33da8bd52ee56c0d3e0b0fee3d5747340350c8a0
Sars4 <logansars225@gmail.com>
2024-01-25 22:49:57 -0500

    Added colors

--- CreatePlot.py
+++ CreatePlot.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+import createStockData
 
 # Load spreadsheet
 xl = pd.ExcelFile('./Data/NVDAData.xlsx')
-
-# Load a sheet into a DataFrame
 df = xl.parse('Sheet')
 
-# Assume that 'Column1' and 'Column2' are the columns you want to plot
+colors = np.where(df['Closing'].diff() > 0, 'green', 'red')
+
+# Find the columns
 plt.plot(df['Date'], df['Closing'])
+for i in range(1, len(df)):
+    plt.plot(df['Date'].iloc[i-1:i+1], df['Closing'].iloc[i-1:i+1], color=colors[i-1])
 
 # Display the plot
 plt.show()
