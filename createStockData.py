520a36e253c514b91e750723a64a6ec092f70d6c
Sars4 <logansars225@gmail.com>
2024-01-25 21:59:27 -0500

    Basic stock data

--- createStockData.py
+++ createStockData.py
@@ -1,6 +1,45 @@
+import warnings
 import yfinance as yf
+from openpyxl import Workbook
+
+# Ignore FutureWarning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Creating an Excel Sheet
+wb = Workbook()
+ws = wb.active
+
+# Stock name
+stock = "NVDA"
 
 
-data = yf.download("NVDA", period = "1mo")
+
+# Create a Ticker object for the stock
+nvda = yf.Ticker(stock)
+
+# Get the historical market data
+hist = nvda.history(period="5y")
+
+# Remove timezone from dates
+hist.index = hist.index.tz_localize(None)
+
+# Add dates to excel sheet
+ws['A1'] = "Date"
+for i in range(len(hist)):
+    ws['A'+ str(i+2)] = hist.index[i]
+
+# Add closing prices to excel sheet
+ws['B1'] = "Closing"
+for i in range(len(hist)):
+    ws['B'+ str(i+2)] = hist['Close'].iloc[i]
+
+# Add percentage change
+ws['C1'] = "Pct Change"
+hist['Percentage Change'] = hist['Close'].pct_change() * 100
+for i in range(len(hist)):
+    ws['C'+ str(i+2)] = hist['Percentage Change'].iloc[i]
 
 
+
+
+wb.save(stock + 'Data.xlsx')
